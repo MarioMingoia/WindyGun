71b85a69c249f64e5a32974391d7f91acc9233ff
MarioMingoia <MM3417@live.mdx.ac.uk>
2022-02-28 13:36:10 +0000

    Project
    All of Project so far

---
+++ GAM3002 - Windy Gun/Assets/Scripts/Environment/ConveyorBelt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public GameObject belt;
+    public List<string> tags;
+    public Transform endPoint;
+    public float speed;
+    public GameObject newPos;
+    private void OnTriggerStay(Collider other)
+    {
+        for (int i = 0; i < tags.Count; i++)
+        {
+            if (other.gameObject.CompareTag(tags[i]))
+            {
+                other.transform.position = Vector3.MoveTowards(other.transform.position, endPoint.position, speed * Time.deltaTime);
+
+                if (Vector3.Distance(other.transform.position, endPoint.position) <= 1f)
+                {
+                    print("123");
+                    other.transform.position = newPos.transform.position;
+                }
+
+            }
+
+        }
+
+       
+
+    }
+}
---
+++ GAM3002 - Windy Gun/Assets/Scripts/Environment/winchScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winchScript : MonoBehaviour
+{
+
+    public GameObject myCenter;
+
+    public float myStrength;
+
+    public LineRenderer lrnd;
+
+    public GameObject platform;
+
+    public GameObject target;
+    public bool windLeft = false;
+
+    public Vector3 platOriginalPos;
+
+    public List<GameObject> windy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lrnd = GetComponent<LineRenderer>();
+
+        platOriginalPos = platform.transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lrnd.SetPosition(0, myCenter.transform.position);
+        lrnd.SetPosition(1, platform.transform.position);
+        if (myStrength > 0)
+        {
+
+            if (!Vector3.Equals(platform.transform.position.y, target.transform.position.y))
+            {
+                transform.GetChild(0).transform.RotateAround(myCenter.transform.position, transform.right, myStrength * Time.deltaTime);
+            }
+            platform.transform.localPosition = Vector3.MoveTowards(platform.transform.localPosition, target.transform.localPosition, myStrength / 10 * Time.deltaTime);
+        }
+
+        for (int i = 0; i < windy.Count; i++)
+        {
+            if (windy[i] == null)
+            {
+                windy.RemoveAt(i);
+                windLeft = true;
+                myStrength = 0;
+            }
+        }
+
+        if (windLeft == true && windy.Count <= 0 && myStrength <= 0)
+        {
+            transform.GetChild(0).transform.RotateAround(myCenter.transform.position, transform.right, -15 * Time.deltaTime);
+            platform.transform.localPosition = Vector3.MoveTowards(platform.transform.localPosition, platOriginalPos, 15 * Time.deltaTime);
+
+            if (Vector3.Equals(platform.transform.localPosition, platOriginalPos))
+                windLeft = false;
+
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name.Contains("WindArea"))
+        {
+            myStrength = other.gameObject.GetComponent<windArea>().strength;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("WindArea"))
+        {
+            windy.Add(other.gameObject);
+            windLeft = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
